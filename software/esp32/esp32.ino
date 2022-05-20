d789e8945dc047cb89be85e530df8d0cb155732b
MousaAlsayegh <amousa@pdx.edu>
2022-05-19 22:10:55 -0700

    From Wednesday  05/18/2022 meeting

--- software/esp32/esp32.ino
+++ software/esp32/esp32.ino
@@ -7,32 +7,7 @@
  *  
  *  
  */
-/*
- WiFi Web Server LED Blink
 
- A simple web server that lets you blink an LED via the web.
- This sketch will print the IP address of your WiFi Shield (once connected)
- to the Serial monitor. From there, you can open that address in a web browser
- to turn on and off the LED on pin 5.
-
- If the IP address of your shield is yourAddress:
- http://yourAddress/H turns the LED on
- http://yourAddress/L turns it off
-
- This example is written for a network using WPA encryption. For
- WEP or WPA, change the Wifi.begin() call accordingly.
-
- Circuit:
- * WiFi shield attached
- * LED attached to pin 5
-
- created for arduino 25 Nov 2012
- by Tom Igoe
-
-ported for sparkfun esp32 
-31.01.2017 by Jan Hendrik Berlin
- 
- */
 #include <WiFi.h>
 #include <WiFiMulti.h>
 #include <Wire.h>
@@ -53,23 +28,23 @@ WiFiMulti WiFiMulti;
 #define MOSI 23  //MOSI
 #define SCLK 18  //SCLK
 
-//Inputs
-#define in4 34   //GPIO 34
+//Commented inputs are taken
+//#define in4 34   //GPIO 34
 #define in5 36   //GPIO 36
 #define in6 39   //GPIO 39
 
-//Outputs
+//Commented outputs are taken
 #define out1 4   //GPIO 4 Buzzer  
-//#define out2 12  //GPIO 12  Green
-//#define out3 13  //GPIO 13  Red
-#define out4 14  //GPIO 14
-#define out5 15  //GPIO 15
-#define out6 21  //GPIO 21
+//#define out2 12  //GPIO 12  Green_status
+//#define out3 13  //GPIO 13  Red_status
+//#define out4 14  //GPIO 14    Red_power
+//#define out5 15  //GPIO 15    Green_power
+//#define out6 21  //GPIO 21    Blue_power
 #define out7 25  //GPIO 25
 #define out8 26  //GPIO 26
 #define out9 27  //GPIO 27
 #define out10 32 //GPIO 32
-//#define out11 33 //GPIO 33  Blue
+//#define out11 33 //GPIO 33  Blue_status
 
 void setup()
 {   
